d61cc831c4eecd5c1cdd432997996b7a75efe0a0
Ahmed Shahid <a.razashahid19@gmail.com>
2024-11-21 02:35:01 +0500

    Initialized expo app and made layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,120 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { myColors } from "@/constants/Colors";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import profilePic from "../../assets/images/profilePic.jpeg";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+    <View
+      style={{
+        backgroundColor: myColors.colorMain,
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      {/* Header View Start*/}
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          borderWidth: 2,
+          borderColor: myColors.colorSecondary,
+          width: "90%",
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={profilePic}
+            style={{
+              width: "65%",
+              height: "40%",
+              borderRadius: 99999,
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            flex: 2,
+            alignItems: "flex-start",
+            justifyContent: "center",
+            gap: 4,
+            paddingHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 12,
+              color: "grey",
+              fontFamily: "SpaceMono",
+            }}
+          >
+            Welcome Back,
+          </Text>
+
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: "SpaceMono",
+              fontWeight: 600,
+            }}
+          >
+            Ahmed Raza
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <MaterialIcons
+            name="logout"
+            size={32}
+            color={myColors.colorSecondary}
+            style={{ marginTop: 20 }}
+          />
+          {/* <Text
+            style={{
+              fontSize: 10,
+              color: "grey",
+              fontFamily: "",
+            }}
+          >
+            Logout
+          </Text> */}
+        </View>
+      </View>
+      {/* HEader View End */}
+
+      <View
+        style={{
+          flex: 3,
+          borderWidth: 2,
+          borderColor: myColors.colorSecondary,
+          width: "90%",
         }}
-      />
-    </Tabs>
+      ></View>
+      <View
+        style={{
+          flex: 2,
+          borderWidth: 2,
+          borderColor: myColors.colorSecondary,
+          width: "90%",
+        }}
+      ></View>
+    </View>
   );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,32 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#524EE5";
+const tintColorDark = "#524EE5";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#524EE5",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
+};
+
+export const myColors = {
+  colorMain: "#ffffff",
+  colorPrimary: "#C8FFA6",
+  colorSecondary: "#524EE5",
+  textPrimary: "#000000",
+  textSecondary: "#ffffff",
 };
