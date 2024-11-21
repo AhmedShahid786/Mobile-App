8168427a579d978ea90c34637236852f5eff2c5a
Ahmed Shahid <a.razashahid19@gmail.com>
2024-11-21 16:55:53 +0500

    Updated The Layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import { myColors } from "@/constants/Colors";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-
-import profilePic from "../../assets/images/profilePic.jpeg";
+import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
 
 export default function TabLayout() {
   return (
@@ -16,100 +15,12 @@ export default function TabLayout() {
         justifyContent: "space-around",
       }}
     >
-      {/* Header View Start*/}
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          borderWidth: 2,
-          borderColor: myColors.colorSecondary,
-          width: "90%",
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Image
-            source={profilePic}
-            style={{
-              width: "65%",
-              height: "40%",
-              borderRadius: 99999,
-            }}
-          />
-        </View>
-
-        <View
-          style={{
-            flex: 2,
-            alignItems: "flex-start",
-            justifyContent: "center",
-            gap: 4,
-            paddingHorizontal: 10,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 12,
-              color: "grey",
-              fontFamily: "SpaceMono",
-            }}
-          >
-            Welcome Back,
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 18,
-              fontFamily: "SpaceMono",
-              fontWeight: 600,
-            }}
-          >
-            Ahmed Raza
-          </Text>
-        </View>
-
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <MaterialIcons
-            name="logout"
-            size={32}
-            color={myColors.colorSecondary}
-            style={{ marginTop: 20 }}
-          />
-          {/* <Text
-            style={{
-              fontSize: 10,
-              color: "grey",
-              fontFamily: "",
-            }}
-          >
-            Logout
-          </Text> */}
-        </View>
-      </View>
-      {/* HEader View End */}
+      <Header />
+      <Hero />
 
       <View
         style={{
           flex: 3,
-          borderWidth: 2,
-          borderColor: myColors.colorSecondary,
-          width: "90%",
-        }}
-      ></View>
-      <View
-        style={{
-          flex: 2,
           borderWidth: 2,
           borderColor: myColors.colorSecondary,
           width: "90%",
